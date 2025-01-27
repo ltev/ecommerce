8fcd2611b955a309a12a491031797dc9fdbbef6a
ltev <ltev@interia.pl>
2025-01-27 18:17:54 +0100

    Added @CrossOrigin for Angular frontend access

--- backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/dao/ProductCategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.ltev.ecommerce.dao;
 import com.ltev.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")       // accept calls from web browser scripts for this origin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.ltev.ecommerce.dao;
 
 import com.ltev.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")       // accept calls from web browser scripts for this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
