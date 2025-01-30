f02f7e32f58e8f966455bd860322f460f67fbec6
ltev <ltev@interia.pl>
2025-01-30 12:38:10 +0100

    Modifying Spring Boot to expose entity ids

--- backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/config/DataRestConfig.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/config/DataRestConfig.java
@@ -2,14 +2,24 @@ package com.ltev.ecommerce.config;
 
 import com.ltev.ecommerce.entity.Product;
 import com.ltev.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
+@AllArgsConstructor
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -22,5 +32,23 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                     .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
                     .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
         }
+
+        // call an internal helper method
+        exposeIdsForAllEntityClasses(config);
+    }
+
+    private void exposeIdsForAllEntityClasses(RepositoryRestConfiguration config) {
+        // list all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        /// create an array of the entity types
+        List<Class<?>> entityClasses = new ArrayList<>();
+        for (EntityType<?> entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(Class[]::new);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ltev/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,18 @@
 package com.ltev.ecommerce.dao;
 
 import com.ltev.ecommerce.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @CrossOrigin("http://localhost:4200")       // accept calls from web browser scripts for this origin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * Spring Data REST endpoint: /api/products/search/findByCategoryId?id=:id
+     */
+    List<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
