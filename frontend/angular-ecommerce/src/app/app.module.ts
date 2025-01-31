58cfea4a87712ceb6db706b722a7299188c093de
ltev <ltev@interia.pl>
2025-01-31 13:50:52 +0100

    Create new component for search. Add new Angular route for searching

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -8,8 +8,10 @@ import { HttpClientModule } from '@angular/common/http'
 import { ProductService } from './services/product.service';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [    // list order check - first match wins
+  {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id/:categoryName', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
@@ -21,7 +23,8 @@ const routes: Routes = [    // list order check - first match wins
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent
+    ProductCategoryMenuComponent,
+    SearchComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
