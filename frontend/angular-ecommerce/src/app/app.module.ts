adf371b23ddf8f78c65e6262e00770013f9aff2e
ltev <ltev@interia.pl>
2025-02-04 11:50:26 +0100

    New component cartStatus

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 const routes: Routes = [    // list order check - first match wins
   {path: 'search/:keyword', component: ProductListComponent},
@@ -30,7 +31,8 @@ const routes: Routes = [    // list order check - first match wins
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
